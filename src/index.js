6c9818685f69dce9e0048463aca8ba50997a08af
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-09-20 01:12:37 +0530

    Setup opf smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = async () => {
 
     // create the express object
@@ -12,8 +14,15 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, async () => {
 
-        console.log(`Server started at ${PORT}`);
-    });
+    console.log(`Server started at ${PORT}`);
+    
+    sendBasicEmail(
+        'support@admin.com',
+        'ayush2992@gmail.com',
+        'This is a testing email',
+        'Hey, how are you, I hope you like the support'
+    );
+});
 }
 
 setupAndStartServer();
