b7f523b8a7c66cb94d2f4560abeb4f1b1cf95a5c
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-09-20 20:17:28 +0530

    Added RabbitMQ and message Queue logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
+const { createChannel } = require("./utils/messageQueue");
+
+//const {sendBasicEmail} = require('./services/email-service');
 
 const TicketController = require('./controllers/ticket-controller');
 
@@ -14,11 +17,14 @@ const setupAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    const channel = await createChannel();
+
     app.post('/api/v1/tickets', TicketController.create);
     app.listen(PORT, async () => {
 
     console.log(`Server started at ${PORT}`);
-    jobs();
+    //jobs();
     // sendBasicEmail(
     //     'support@admin.com',
     //     'ayush2992@gmail.com',
